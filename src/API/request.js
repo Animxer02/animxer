836ac4aee2d6a3b8eda7a864786a40117f0ea185
Animxer02 <159162999+Animxer02@users.noreply.github.com>
2024-02-08 09:08:23 +0800

    Update request.js

--- src/API/request.js
+++ src/API/request.js
@@ -1,5 +1,4 @@
 const baseUrl = "https://animxer-api-six.vercel.app/anime/gogoanime";
-const baseUrl = "https://anim-xer-chi.vercel.app/anime/gogoanime";
 
 export const makeRequest = async (endpoint, config) => {
     const url = `${baseUrl}${endpoint}`;
